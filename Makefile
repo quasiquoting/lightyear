a56fe41a02bc50e5855c5abd9dfc21b531b7b056
Eric Bailey <eric@ericb.me>
2016-08-19 01:15:50 -0500

    Housekeeping
    - Flesh out lightyear.ipkg
    - Adopt my usual Makefile
    - Move source code into src/
    - Ensure tests/runtests.sh is not wider than 80 characters

--- Makefile
+++ Makefile
@@ -1,14 +1,30 @@
-all: install
+IDRIS := idris
+PKG   := lightyear
+
+.PHONY: all clean clean-all install rebuild doc doc-clean test
+
+all: build
+
+build: src/Lightyear/*.idr
+	@$(IDRIS) --build $(PKG).ipkg
+
+clean:
+	@$(IDRIS) --clean $(PKG).ipkg
+	@find . -name '*.ibc' -delete
+
+clean-all: clean doc-clean
 
 install: build
-	idris --install lightyear.ipkg
+	@$(IDRIS) --install $(PKG).ipkg
 
-build: Lightyear/*.idr
-	idris --build lightyear.ipkg
+rebuild: clean build
+
+docs: docs-clean build
+	@$(IDRIS) --mkdoc $(PKG).ipkg \
+	&& mv $(PKG)_doc docs
+
+docs-clean:
+	@rm -rf docs >/dev/null
 
 test: install
 	(cd tests; bash runtests.sh)
-
-clean:
-	idris --clean lightyear.ipkg
-	rm tests/*.ibc
--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!usr/bin/env bash
 
 die() {
 	echo "$1" >&2
@@ -12,13 +12,16 @@ clean_up() {
 clean_up
 
 echo "compiling lightyear tests..."
-idris Test.idr -p lightyear -o test || die "* could not compile tests *"
+idris Test.idr -p lightyear -o test \
+    || die "* could not compile tests *"
 
 echo "compiled OK, running lightyear tests..."
-timeout 5s ./test > output || die "* test failed or timed out *"
+timeout 5s ./test > output \
+    || die "* test failed or timed out *"
 
 echo "compiling the JSON test..."
-idris JsonTest.idr -p lightyear -p contrib -o json || die "* could not compile the json test *"
+idris JsonTest.idr -p lightyear -p contrib -o json \
+        || die "* could not compile the json test *"
 
 echo "compiled OK, running the JSON test..."
 timeout 5s ./json >> output || die "* test failed or timed out *"
